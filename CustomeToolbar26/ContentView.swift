a03ac60fef7693a1da5dbffeeedda722d29921eb
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-03-05 12:42:24 +0530

    Initial Commit

---
+++ CustomeToolbar26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomeToolbar26
+//
+//  Created by Tanmaydeep singh on 05/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomeToolbar26/CustomeToolbar26App.swift
@@ -0,0 +1,17 @@
+//
+//  CustomeToolbar26App.swift
+//  CustomeToolbar26
+//
+//  Created by Tanmaydeep singh on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomeToolbar26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
